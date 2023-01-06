ff0c5ec4c8ab473dd9d69a0813e2db2f4a59a9ad
Gabriel Fynecontry <27024731+fynecontry@users.noreply.github.com>
2023-01-06 16:48:38 +0000

    Ascii art of adjacent pyramid of blocks of certain height.
    Where height is user supplied.

---
+++ C/week1_C/mario-v2.c
@@ -0,0 +1,41 @@
+/*
+Displays ascii art of adjacent pyramids of certain height.
+Where n - is supplied by user.
+
+inspired by Mario Brothers.
+Towards the end of the world 1-1 Mario must hop over adjacent pyramids pyramid of blocks
+*/
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height;
+    // prompt user for height between 1 and 8 inclusive
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < 1 || height > 8);
+
+    // Loop through height
+    for (int i = 0; i < height; i++)
+    {
+        // print art for rows
+        for (int j = 1; j < height - i; j++)
+        {
+            printf(" ");
+        }
+        // print art for reversed shape
+        for (int k = 0; k <= i; k++)
+        {
+            printf("#");
+        }
+        printf("  ");
+        for (int l = 0; l <= i; l++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
