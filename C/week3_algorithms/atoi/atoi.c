95a61c5805273d661fa8873f24e7625cb1ca6133
Gabriel Fynecontry <27024731+fynecontry@users.noreply.github.com>
2023-01-18 06:31:43 +0000

    Recursive implementation of atoi()

---
+++ C/week3_algorithms/atoi/atoi.c
@@ -0,0 +1,40 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+int convert(string input);
+
+int main(void)
+{
+    string input = get_string("Enter a positive integer: ");
+
+    for (int i = 0, n = strlen(input); i < n; i++)
+    {
+        if (!isdigit(input[i]))
+        {
+            printf("Invalid Input!\n");
+            return 1;
+        }
+    }
+
+    // Convert string to int
+    printf("%i\n", convert(input));
+}
+
+
+int convert(string input)
+{
+    // Returns interger value of a passed string
+
+    // base case if NULL is at current index
+    if (input[strlen(input) - 1] == '\0')
+    {
+        return 0;
+    }
+    // Retrieve interger value of char by subtracting by '0'
+    int num = input[strlen(input) - 1] - '0';
+    input[strlen(input) - 1] = '\0';
+    return num + 10 * convert(input);
+}
