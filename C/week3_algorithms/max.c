73320be91acb84b4d47f04ed4d5d354a3511a268
Gabriel Fynecontry <27024731+fynecontry@users.noreply.github.com>
2023-01-30 05:45:54 +0000

    Find the max value in an array

---
+++ C/week3_algorithms/max.c
@@ -0,0 +1,42 @@
+// Practice writing a function to find a max value
+
+#include <cs50.h>
+#include <stdio.h>
+
+int max(int array[], int n);
+
+int main(void)
+{
+    int n;
+    // Prompt user for number of elements
+    do
+    {
+        n = get_int("Number of elements: ");
+    }
+    while (n < 1);
+
+    int arr[n];
+
+    // Search for maximum value
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = get_int("Element %i: ", i);
+    }
+
+    // Display maximum value
+    printf("The max value is %i.\n", max(arr, n));
+}
+
+// returns the max value
+int max(int array[], int n)
+{
+    int temp_max = array[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (array[i] > temp_max)
+        {
+            temp_max = array[i];
+        }
+    }
+    return temp_max;
+}
